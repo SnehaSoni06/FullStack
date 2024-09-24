86f3c099e79cbeb69e706f3a897b15b9357237d0
SnehaSoni06 <snehasonivns@gmail.com>
2024-09-24 13:37:41 +0530

    commit

--- client/src/components/details/DetailView.jsx
+++ client/src/components/details/DetailView.jsx
@@ -1,7 +1,17 @@
-
-
+import {useEffect} from 'react';
+import {useDispatch} from 'react-redux';
+import {useParams} from 'react-router-dom';  //to take out anything from url, useParams is used
+import { getProductDetails } from '../../redux/actions/productActions';
 
 const DetailView=()=>{
+
+  const dispatch=useDispatch();
+  const {id} =useParams();
+
+   useEffect(()=>{
+    dispatch(getProductDetails(id))
+ }, [dispatch ,id])
+    
     return(
         <div>
            Hello 
--- client/src/redux/reducers/productReducer.js
+++ client/src/redux/reducers/productReducer.js
@@ -11,6 +11,24 @@ import * as actionTypes from '../constants/productConstant';
                     return state
 
     }
-    
-    
  }
+
+ export const getProductDetailsReducer=(state={product:{}},action)=>{  //product:{} bcs we are taking detail of a single product
+
+ switch(action.type){
+    case actionTypes.GET_PRODUCT_DETAILS_REQUEST:
+        return{loading:true}
+
+    case actionTypes.GET_PRODUCT_DETAILS_SUCCESS:
+        return{loading:false, product:action.payload}
+
+    case actionTypes.GET_PRODUCT_DETAILS_FAIL:
+        return{loading:false,error:action.payload}
+
+    case actionTypes.GET_PRODUCT_DETAILS_RESET:
+            return{product:{}}         //initial state 
+
+        default:
+            return state
+ }
+}
